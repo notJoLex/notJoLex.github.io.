eeb08b34d9f26d8603044b4da04ee32b3ec48505
notJoLex <Johann.alexander.arendt@gmail.com>
2025-05-19 19:14:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// Alle Boxen mit der Klasse 'box' auswählen
+const boxes = document.querySelectorAll('.box, .return-button');
+
+// Für jede Box ein Klick-Ereignis hinzufügen
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
+    // Den Wert aus dem data-link Attribut lesen
+    const link = box.getAttribute("data-link");
+
+    // Wenn ein Link da ist, weiterleiten zur Seite
+    if (link) {
+      window.location.href = link;
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
